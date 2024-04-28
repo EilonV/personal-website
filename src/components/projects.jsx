7dfe1678c9b7da217078efb0c79d33efdc739c56
EilonV <73593619+EilonV@users.noreply.github.com>
2024-04-29 00:49:17 +0300

    projects style

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,23 +3,43 @@ export const Projects = () => {
         <h2>PROCJETS</h2>
         <div className="grid">
             <div>
-                <div className="project">
-                    <h3>project title</h3>
-                    <p>project desc</p>
+                <div className="project-wrapper">
+                    <div className="project">
+                        <div className="info">
+                            <h3>project title</h3>
+                            <p>project desc</p>
+                        </div>
+                        <a href="">check it out</a>
+                    </div>
                 </div>
-                <div className="project">
-                    <h3>project title</h3>
-                    <p>project desc</p>
+                <div className="project-wrapper">
+                    <div className="project">
+                        <div className="info">
+                            <h3>project title</h3>
+                            <p>project desc</p>
+                        </div>
+                        <a href="">check it out</a>
+                    </div>
                 </div>
             </div>
             <div>
-                <div className="project">
-                    <h3>project title</h3>
-                    <p>project desc</p>
+                <div className="project-wrapper">
+                    <div className="project">
+                        <div className="info">
+                            <h3>project title</h3>
+                            <p>project desc</p>
+                        </div>
+                        <a href="">check it out</a>
+                    </div>
                 </div>
-                <div className="project">
-                    <h3>project title</h3>
-                    <p>project desc</p>
+                <div className="project-wrapper">
+                    <div className="project">
+                        <div className="info">
+                            <h3>project title</h3>
+                            <p>project desc</p>
+                        </div>
+                        <a href="">check it out</a>
+                    </div>
                 </div>
             </div>
         </div>
