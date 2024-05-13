ebd6b3001dc666bcd196aa5f6c863eef704019c2
EilonV <73593619+EilonV@users.noreply.github.com>
2024-05-13 13:15:43 +0300

    google analytics

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,6 +1,10 @@
 import { Home } from "./pages/home";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize('G-6069HFH3HY')
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
   return (
     <Home />
   )
